cb139460f4c9aa2d0e1f1cd51a99223e9751497c
Tyler.Yin <yinzhiyongyzy@126.com>
2017-04-08 22:17:21 +0800

    提交demo

--- demo/demo/ViewController.swift
+++ demo/demo/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var colorTextField: UITextField!
+    @IBOutlet weak var colorButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func changeColorAction(_ sender: UIButton) {
+        colorView.backgroundColor = UIColor.init(hexColor: colorTextField.text ?? "#f76299")
+    }
+    
+    @IBAction func randColorAction(_ sender: UIButton) {
+        colorView.backgroundColor = UIColor.randColor()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
